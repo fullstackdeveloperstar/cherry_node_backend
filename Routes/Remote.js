5457e43d040ab47670bba66f79f829ed13ef1912
calinblitiu <30364734+calinblitiu@users.noreply.github.com>
2018-09-14 01:27:26 -0700

    hashcode backend

--- Routes/Remote.js
+++ Routes/Remote.js
@@ -370,6 +370,54 @@ remotedb.get('/getUserInterestMovie/:id', function(req,res) {
     }); 
 });
 
+remotedb.get('/getUserHashCodes/:id', function(req,res) {
+   var appData = {};
+
+    database.connection.getConnection(function(err, connection) {
+        if (err) {
+            appData["error"] = 1;
+            appData["data"] = "Internal Server Error";
+            res.status(500).json(appData);
+        } else {
+            connection.query('SELECT *FROM pheramor_user_hash_tags where user_id='+req.params.id, function(err, rows, fields) {
+                if (!err) {
+                    appData["error"] = 0;
+                    appData["data"] = rows;
+                    res.status(200).json(appData);
+                } else {
+                    appData["data"] = "No data found";
+                    res.status(204).json(appData);
+                }
+            });
+            connection.release();
+        }
+    }); 
+});
+
+remotedb.get('/getUserHashCode/:id', function(req,res) {
+   var appData = {};
+
+    database.connection.getConnection(function(err, connection) {
+        if (err) {
+            appData["error"] = 1;
+            appData["data"] = "Internal Server Error";
+            res.status(500).json(appData);
+        } else {
+            connection.query('SELECT *FROM pheramor_hashtags where id='+req.params.id, function(err, rows, fields) {
+                if (!err) {
+                    appData["error"] = 0;
+                    appData["data"] = rows;
+                    res.status(200).json(appData);
+                } else {
+                    appData["data"] = "No data found";
+                    res.status(204).json(appData);
+                }
+            });
+            connection.release();
+        }
+    }); 
+});
+
 
 
 module.exports = remotedb;
